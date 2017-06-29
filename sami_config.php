8793907200be17cd2ebd152bb3196c1ae2a3afbb
Riikka Kalliomäki <riikka.kalliomaki@gmail.com>
2017-06-29 15:22:22 +0300

    Update API documentation

--- sami_config.php
+++ sami_config.php
@@ -9,8 +9,16 @@ $iterator = Finder::create()
     ->name('*.php')
     ->in(__DIR__ . '/src');
 
-return new Sami($iterator, [
-    'title'                => 'Streaming JSON Encoder',
+$theme = getenv('SAMI_THEME');
+$settings = [];
+
+if ($theme) {
+    $settings['theme'] = basename($theme);
+    $settings['template_dirs'] = [dirname($theme)];
+}
+
+return new Sami($iterator, $settings + [
+    'title'                => 'Streaming JSON Encoder API',
     'build_dir'            => __DIR__ . '/build/doc',
     'cache_dir'            => __DIR__ . '/build/cache',
     'remote_repository'    => new GitHubRemoteRepository('violet-php/streaming-json-encoder', __DIR__),
