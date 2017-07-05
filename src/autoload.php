23a91495a5d201d571f99200cfcb1700a1bae5a6
Riikka Kalliomäki <riikka.kalliomaki@gmail.com>
2017-07-05 13:40:20 +0300

    Provide a clarifying comment

--- src/autoload.php
+++ src/autoload.php
@@ -2,6 +2,7 @@
 
 namespace Violet\StreamingJsonEncoder;
 
+// Bundled autoloader provided as an optional alternative to composer autoloader
 spl_autoload_register(function ($class) {
     if (strncmp($class, __NAMESPACE__, strlen(__NAMESPACE__)) === 0) {
         $path = __DIR__ . strtr(substr($class, strlen(__NAMESPACE__)), ['\\' => DIRECTORY_SEPARATOR]) . '.php';
